44264a0995c7eb1d5608c212de802d9f4af8c79b
bencallis1 <you@example.com>
2014-12-29 13:57:48 -0700

    second commit

---
+++ app/js/controllers/posCtrl.js
@@ -0,0 +1,16 @@
+app.controller('posCtrl', function($scope) {
+
+
+$scope.order="New Order";
+$scope.sale="New Sale";
+$scope.quote="New Quote";
+$scope.inventory="Inventory";
+$scope.functions="Functions";
+$scope.item1="test from controller";
+$scope.app.name="ActionTrak";
+$scope.item
+$scope.item
+$scope.item
+$scope.item
+$scope.item
+});
